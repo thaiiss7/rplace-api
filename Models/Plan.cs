74212638398e999014fb7ac3a931f14c3b826f91
thaiiss7 <thaismichel7@gmail.com>
2025-08-08 13:25:11 -0300

    add models

--- Models/Plan.cs
+++ Models/Plan.cs
@@ -5,5 +5,6 @@ public class Plan
     public Guid ID { get; set; }
     public Guid ProfileId { get; set; }
     public Profile PlanProfile { get; set; }
-
+    public Guid GiftCardId { get; set; }
+    public GiftCard GiftCard { get; set; }
 }
