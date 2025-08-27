8a5fa3085be3a6cf5eae65d82efd0d2f2c39fb84
thaiiss7 <thaismichel7@gmail.com>
2025-08-27 16:04:47 -0300

    some usecases

--- UseCase/AcceptInvite/AcceptInviteUseCase.cs
+++ UseCase/AcceptInvite/AcceptInviteUseCase.cs
@@ -1,9 +1,20 @@
+using Microsoft.EntityFrameworkCore;
+using Rplace.Models;
+
 namespace Rplace.UseCase.AcceptInvite;
 
-public class AcceptInviteUseCase
+public class AcceptInviteUseCase(rplaceDbContext ctx)
 {
     public async Task<Result<AcceptInviteResponse>> Do(AcceptInvitePayload payload)
     {
+        //procura o banco no banco de dados o convite com o id passado
+        var invite = await ctx.Invites.FirstOrDefaultAsync(i => i.ID == payload.InviteId);
+        //acessa o booleano do convite
+        //coloca no booleano o mesmo valor passado no payload
+        invite.Accepted = payload.Accept;
+        //salva as alterações no banco de dados
+        await ctx.SaveChangesAsync();
+        
         return Result<AcceptInviteResponse>.Success(null);
     }
 }
