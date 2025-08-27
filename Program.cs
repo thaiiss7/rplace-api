518b1753287e7190f667b31f320f5ba9b904f14c
thaiiss7 <thaismichel7@gmail.com>
2025-08-27 14:20:31 -0300

    fix version

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 using System.Text;
 using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 using Rplace.Models;
