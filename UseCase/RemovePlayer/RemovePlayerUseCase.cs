4105914af06c96779a37c471518563ee5d1af172
KetyJomes <jomessofiaketlyn@gmail.com>
2025-09-03 14:46:55 -0300

    Fiz UseCases InvitePlayer e RemovePlayer

--- UseCase/RemovePlayer/RemovePlayerUseCase.cs
+++ UseCase/RemovePlayer/RemovePlayerUseCase.cs
@@ -1,9 +1,20 @@
+using Microsoft.EntityFrameworkCore;
+using Rplace.Models;
+
 namespace Rplace.UseCase.RemovePlayer;
 
-public class RemovePlayerUseCase
+public class RemovePlayerUseCase(rplaceDbContext ctx)
 {
     public async Task<Result<RemovePlayerResponse>> Do(RemovePlayerPayload payload)
     {
+        
+        var player = await ctx.Rooms.FirstOrDefaultAsync(r => r.ID == payload.UserId);
+            
+        ctx.Rooms.Remove(player);
+
+        await ctx.SaveChangesAsync();
+
+    
         return Result<RemovePlayerResponse>.Success(null);
     }
 }
