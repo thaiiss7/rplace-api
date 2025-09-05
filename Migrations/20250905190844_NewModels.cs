43c0242f0d0b21a1734b031aae4f048f6469639e
BrunaElohaBonk <brunaelohabonk698@gmail.com>
2025-09-05 16:10:09 -0300

    Atualizei add InitialModel e update novamente

---
+++ Migrations/20250905190844_NewModels.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace rplace_api.Migrations
+{
+    /// <inheritdoc />
+    public partial class NewModels : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
